1f07e741107ff9f5dd8dbf54505b32ce6d4a7f15
amandajane3 <amandajane@snu.ac.kr>
2021-11-01 21:30:45 +0900

    Initial Commit

---
+++ test01/test01App.swift
@@ -0,0 +1,17 @@
+//
+//  test01App.swift
+//  test01
+//
+//  Created by minseo kim on 2021/11/01.
+//
+
+import SwiftUI
+
+@main
+struct test01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
